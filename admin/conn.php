9414593f4d48d2ed778ce640a16764dbe95c1ad5
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-07 23:14:14 -0600

    Update conn.php

--- admin/conn.php
+++ admin/conn.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost'; // El nombre del servidor de la base de datos
-$user = 'root'; // El nombre de usuario de la base de datos
-$password = 'root'; // La contraseña de la base de datos
-$database = 'besoccer'; // El nombre de la base de datos
+$user = 'u4396178_n57whzfm2yvo'; // El nombre de usuario de la base de datos
+$password = 'SlowD2019'; // La contraseña de la base de datos
+$database = 'u4396178_besoccer'; // El nombre de la base de datos
 $conn = mysqli_connect($host, $user, $password, $database);
 
  //Verificar si la conexión fue exitosa
