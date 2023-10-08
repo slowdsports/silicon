05e4014a49dcb3a74a116812fff44dae48641484
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-08 11:29:49 -0600

    Update canales.php

--- inc/componentes/canales.php
+++ inc/componentes/canales.php
@@ -9,7 +9,7 @@ function mostrarCanales($query)
         $canalNombre = $result['canalNombre'];
         $canalCategoria = $result['categoriaNombre'];
         ?>
-        <div class="canal col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div class="canal mycard col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
             <a href="?p=tv&c=<?= $canalId ?>">
                 <div class="card border-0 shadow-sm card-hover card-hover-primary">
                     <div class="card-header">
