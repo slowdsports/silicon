542e9b8e0a830a0d6d0bf14d52fdb316c09a5086
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-07 23:12:55 -0600

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -1,10 +1,11 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "besoccer"); //local
-//$conn = mysqli_connect("localhost","n57whzfm2yvo","SlowD2019","cookiename"); //localHost
+//$conn = mysqli_connect("localhost","root","root","besoccer"); //local
+$conn = mysqli_connect("localhost","u4396178_n57whzfm2yvo","SlowD2019","u4396178_besoccer"); //localHost
 //$conn = mysqli_connect("localhost","iraffle1_root","z1CnB-*XSJ-W","iraffle1_besoccer"); //prod
 
 // Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 ?>
