f6fc0d35c6476168dfc2844db87ee42d08f6476e
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-14 01:23:00 -0600

    Update relacionados.php

--- inc/componentes/relacionados.php
+++ inc/componentes/relacionados.php
@@ -1,15 +1,19 @@
-<section class="container mb-5 pt-md-4">
-    <div class="d-flex flex-sm-row flex-column align-items-center justify-content-between mb-4 pb-1 pb-md-3">
-        <h2 class="h1 mb-sm-0">Canales Relacionados</h2>
-        <a href="blog-grid-with-sidebar.html" class="btn btn-lg btn-outline-primary ms-4">
-            Ver Todo
-            <i class="bx bx-right-arrow-alt ms-1 me-n1 lh-1 lead"></i>
-        </a>
-    </div>
+<?php
+// Verificar si canalTipo está definido y no es nulo
+if (isset($canalTipo) && $canalTipo) {
+    ?>
+    <section class="container mb-5 pt-md-4">
+        <div class="d-flex flex-sm-row flex-column align-items-center justify-content-between mb-4 pb-1 pb-md-3">
+            <h2 class="h1 mb-sm-0">Canales Relacionados</h2>
+            <a href="?p=tv" class="btn btn-lg btn-outline-primary ms-4">
+                Ver Todo
+                <i class="bx bx-right-arrow-alt ms-1 me-n1 lh-1 lead"></i>
+            </a>
+        </div>
 
-    <!-- Carousel -->
-    <div class="swiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden"
-        data-swiper-options="{
+        <!-- Carousel -->
+        <div class="swiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden"
+            data-swiper-options="{
           &quot;slidesPerView&quot;: 1,
           &quot;spaceBetween&quot;: 24,
           &quot;pagination&quot;: {
@@ -31,211 +35,68 @@
             }
           }
         }">
-        <div class="swiper-wrapper" id="swiper-wrapper-4799ce1078136239e" aria-live="polite"
-            style="transform: translate3d(0px, 0px, 0px);">
-
-            <!-- Item -->
-            <div class="swiper-slide h-auto pb-3 swiper-slide-active" style="width: 329.5px; margin-right: 24px;"
-                role="group" aria-label="1 / 4">
-                <article>
-                    <div class="d-block position-relative rounded-3 mb-3">
-                        <a href="#"
-                            class="btn btn-icon btn-light bg-white btn-sm rounded-circle position-absolute top-0 end-0 zindex-5 me-3 mt-3"
-                            data-bs-toggle="tooltip" data-bs-placement="left" aria-label="Listen later"
-                            data-bs-original-title="Listen later">
-                            <i class="bx bx-bookmark"></i>
-                        </a>
-                        <img src="assets/img/avatar/06.jpg"
-                            class="position-absolute top-0 start-0 rounded-circle zindex-2 mt-3 ms-3" width="48"
-                            alt="Avatar">
-                        <span class="badge bg-dark position-absolute bottom-0 end-0 zindex-2 mb-3 me-3">0:25:43</span>
-                        <a href="blog-podcast.html"
-                            class="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"
-                            aria-label="Listen podcast"></a>
-                        <img src="assets/img/blog/podcasts/04.jpg" class="rounded-3" alt="Image">
-                    </div>
-                    <div class="d-flex align-items-center mb-2">
-                        <a href="#" class="badge fs-sm text-nav bg-secondary text-decoration-none">Digital</a>
-                        <span class="fs-sm text-muted border-start ps-3 ms-3">2 hours ago</span>
-                    </div>
-                    <h3 class="h5">
-                        <a href="blog-podcast.html">IOS and the Changing landscape of Data Privacy</a>
-                    </h3>
-                    <div class="d-flex align-items-center text-muted">
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-like fs-lg me-1"></i>
-                            <span class="fs-sm">4</span>
+            <div class="swiper-wrapper" id="swiper-wrapper-4799ce1078136239e" aria-live="polite"
+                style="transform: translate3d(0px, 0px, 0px);">
+                <?php
+                function mostrarCanales($query)
+                {
+                    global $conn;
+                    $channels = mysqli_query($conn, $query);
+                    while ($result = mysqli_fetch_assoc($channels)) {
+                        $canalId = $result['canalId'];
+                        $canalImg = $result['canalImg'];
+                        $canalNombre = $result['canalNombre'];
+                        $canalCategoria = $result['categoriaNombre'];
+                        ?>
+                        <!-- Item -->
+                        <div class="swiper-slide h-auto pb-3 swiper-slide-active" style="width: 329.5px; margin-right: 24px;"
+                            role="group" aria-label="1 / 4">
+                            <article>
+                                <div class="d-block position-relative rounded-3 mb-3">
+                                    <span class="badge bg-dark position-absolute bottom-0 end-0 zindex-2 mb-3 me-3">
+                                        <?= $canalCategoria ?>
+                                    </span>
+                                    <a href="?p=tv&c=<?= $canalId ?>"
+                                        class="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"
+                                        aria-label="Listen podcast"></a>
+                                    <img style="height: 50px!important" src="assets/img/canales/<?= $canalImg ?>.png"
+                                        class="card-img-canal" alt="Image">
+                                    <br><br><br><br>
+                                </div>
+                                <h3 class="h5">
+                                    <a href="?p=tv&c=<?= $canalId ?>">
+                                        <?= $canalNombre ?>
+                                    </a>
+                                </h3>
+                                <div class="d-flex align-items-center text-muted">
+                                </div>
+                                <a href="?p=tv&c=<?= $canalId ?>" class="btn btn-link px-0 mt-3">
+                                    <i class="bx bx-play-circle fs-lg me-2"></i>
+                                    Ver ahora
+                                </a>
+                            </article>
                         </div>
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-comment fs-lg me-1"></i>
-                            <span class="fs-sm">2</span>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bx bx-share-alt fs-lg me-1"></i>
-                            <span class="fs-sm">0</span>
-                        </div>
-                    </div>
-                    <a href="blog-podcast.html" class="btn btn-link px-0 mt-3">
-                        <i class="bx bx-play-circle fs-lg me-2"></i>
-                        Listen now
-                    </a>
-                </article>
+                    <?php }
+                }
+                // Sección de Canales
+                $query = "SELECT * FROM canales
+                INNER JOIN categorias ON canales.canalCategoria = categorias.categoriaId
+                WHERE canalCategoria='$categoria'
+                ORDER BY RAND()
+                DESC LIMIT 18";
+                mostrarCanales($query);
+                ?>
             </div>
 
-            <!-- Item -->
-            <div class="swiper-slide h-auto pb-3 swiper-slide-next" style="width: 329.5px; margin-right: 24px;"
-                role="group" aria-label="2 / 4">
-                <article>
-                    <div class="d-block position-relative rounded-3 mb-3">
-                        <a href="#"
-                            class="btn btn-icon btn-light bg-white btn-sm rounded-circle position-absolute top-0 end-0 zindex-5 me-3 mt-3"
-                            data-bs-toggle="tooltip" data-bs-placement="left" aria-label="Listen later"
-                            data-bs-original-title="Listen later">
-                            <i class="bx bx-bookmark"></i>
-                        </a>
-                        <img src="assets/img/avatar/05.jpg"
-                            class="position-absolute top-0 start-0 rounded-circle zindex-2 mt-3 ms-3" width="48"
-                            alt="Avatar">
-                        <span class="badge bg-dark position-absolute bottom-0 end-0 zindex-2 mb-3 me-3">1:12:05</span>
-                        <a href="blog-podcast.html"
-                            class="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"
-                            aria-label="Listen podcast"></a>
-                        <img src="assets/img/blog/podcasts/05.jpg" class="rounded-3" alt="Image">
-                    </div>
-                    <div class="d-flex align-items-center mb-2">
-                        <a href="#" class="badge fs-sm text-nav bg-secondary text-decoration-none">Processes &amp;
-                            Tools</a>
-                        <span class="fs-sm text-muted border-start ps-3 ms-3">10 hours ago</span>
-                    </div>
-                    <h3 class="h5">
-                        <a href="blog-podcast.html">Behavioral Science and Digital Marketing</a>
-                    </h3>
-                    <div class="d-flex align-items-center text-muted">
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-like fs-lg me-1"></i>
-                            <span class="fs-sm">9</span>
-                        </div>
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-comment fs-lg me-1"></i>
-                            <span class="fs-sm">5</span>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bx bx-share-alt fs-lg me-1"></i>
-                            <span class="fs-sm">6</span>
-                        </div>
-                    </div>
-                    <a href="blog-podcast.html" class="btn btn-link px-0 mt-3">
-                        <i class="bx bx-play-circle fs-lg me-2"></i>
-                        Listen now
-                    </a>
-                </article>
+            <!-- Pagination (bullets) -->
+            <div
+                class="swiper-pagination position-relative pt-2 pt-sm-3 mt-4 swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal">
+                <span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button"
+                    aria-label="Go to slide 1" aria-current="true"></span><span class="swiper-pagination-bullet"
+                    tabindex="0" role="button" aria-label="Go to slide 2"></span><span class="swiper-pagination-bullet"
+                    tabindex="0" role="button" aria-label="Go to slide 3"></span>
             </div>
-
-            <!-- Item -->
-            <div class="swiper-slide h-auto pb-3" style="width: 329.5px; margin-right: 24px;" role="group"
-                aria-label="3 / 4">
-                <article>
-                    <div class="d-block position-relative rounded-3 mb-3">
-                        <a href="#"
-                            class="btn btn-icon btn-light bg-white btn-sm rounded-circle position-absolute top-0 end-0 zindex-5 me-3 mt-3"
-                            data-bs-toggle="tooltip" data-bs-placement="left" aria-label="Listen later"
-                            data-bs-original-title="Listen later">
-                            <i class="bx bx-bookmark"></i>
-                        </a>
-                        <img src="assets/img/avatar/01.jpg"
-                            class="position-absolute top-0 start-0 rounded-circle zindex-2 mt-3 ms-3" width="48"
-                            alt="Avatar">
-                        <span class="badge bg-dark position-absolute bottom-0 end-0 zindex-2 mb-3 me-3">0:32:15</span>
-                        <a href="blog-podcast.html"
-                            class="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"
-                            aria-label="Listen podcast"></a>
-                        <img src="assets/img/blog/podcasts/02.jpg" class="rounded-3" alt="Image">
-                    </div>
-                    <div class="d-flex align-items-center mb-2">
-                        <a href="#" class="badge fs-sm text-nav bg-secondary text-decoration-none">Technology</a>
-                        <span class="fs-sm text-muted border-start ps-3 ms-3">10 hours ago</span>
-                    </div>
-                    <h3 class="h5">
-                        <a href="blog-podcast.html">Why You Should Care About Your Competitors</a>
-                    </h3>
-                    <div class="d-flex align-items-center text-muted">
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-like fs-lg me-1"></i>
-                            <span class="fs-sm">45</span>
-                        </div>
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-comment fs-lg me-1"></i>
-                            <span class="fs-sm">12</span>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bx bx-share-alt fs-lg me-1"></i>
-                            <span class="fs-sm">6</span>
-                        </div>
-                    </div>
-                    <a href="blog-podcast.html" class="btn btn-link px-0 mt-3">
-                        <i class="bx bx-play-circle fs-lg me-2"></i>
-                        Listen now
-                    </a>
-                </article>
-            </div>
-
-            <!-- Item -->
-            <div class="swiper-slide h-auto pb-3" role="group" aria-label="4 / 4"
-                style="width: 329.5px; margin-right: 24px;">
-                <article>
-                    <div class="d-block position-relative rounded-3 mb-3">
-                        <a href="#"
-                            class="btn btn-icon btn-light bg-white btn-sm rounded-circle position-absolute top-0 end-0 zindex-5 me-3 mt-3"
-                            data-bs-toggle="tooltip" data-bs-placement="left" aria-label="Listen later"
-                            data-bs-original-title="Listen later">
-                            <i class="bx bx-bookmark"></i>
-                        </a>
-                        <img src="assets/img/avatar/04.jpg"
-                            class="position-absolute top-0 start-0 rounded-circle zindex-2 mt-3 ms-3" width="48"
-                            alt="Avatar">
-                        <span class="badge bg-dark position-absolute bottom-0 end-0 zindex-2 mb-3 me-3">0:48:02</span>
-                        <a href="blog-podcast.html"
-                            class="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"
-                            aria-label="Listen podcast"></a>
-                        <img src="assets/img/blog/podcasts/03.jpg" class="rounded-3" alt="Image">
-                    </div>
-                    <div class="d-flex align-items-center mb-2">
-                        <a href="#" class="badge fs-sm text-nav bg-secondary text-decoration-none">Marketing</a>
-                        <span class="fs-sm text-muted border-start ps-3 ms-3">1 day ago</span>
-                    </div>
-                    <h3 class="h5">
-                        <a href="blog-podcast.html">Tips for Marketing Managers: How to Maximise Leads</a>
-                    </h3>
-                    <div class="d-flex align-items-center text-muted">
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-like fs-lg me-1"></i>
-                            <span class="fs-sm">10</span>
-                        </div>
-                        <div class="d-flex align-items-center me-3">
-                            <i class="bx bx-comment fs-lg me-1"></i>
-                            <span class="fs-sm">4</span>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bx bx-share-alt fs-lg me-1"></i>
-                            <span class="fs-sm">6</span>
-                        </div>
-                    </div>
-                    <a href="blog-podcast.html" class="btn btn-link px-0 mt-3">
-                        <i class="bx bx-play-circle fs-lg me-2"></i>
-                        Listen now
-                    </a>
-                </article>
-            </div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
         </div>
-
-        <!-- Pagination (bullets) -->
-        <div
-            class="swiper-pagination position-relative pt-2 pt-sm-3 mt-4 swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal">
-            <span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button"
-                aria-label="Go to slide 1" aria-current="true"></span><span class="swiper-pagination-bullet"
-                tabindex="0" role="button" aria-label="Go to slide 2"></span><span class="swiper-pagination-bullet"
-                tabindex="0" role="button" aria-label="Go to slide 3"></span>
-        </div>
-        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
-    </div>
-</section>
+    </section>
+<?php } ?>
