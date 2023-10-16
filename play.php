8d8e9ea584867c7d2077608a5ab7f8ed9afe7962
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-16 03:48:13 -0600

    Update play.php

--- play.php
+++ play.php
@@ -55,7 +55,7 @@ if (isset($_GET['title'])) {
             <!-- Reproductor -->
             <div class="gallery mb-4 pb-2">
                 <a id="playerFake"
-                href="https://abolishmentglum.com/mkd1fhhe?key=81193c57b7f58377107604b71a3e49aa" target="_blank">
+                href="https://www.highcpmrevenuegate.com/mkd1fhhe?key=81193c57b7f58377107604b71a3e49aa" target="_blank">
                     <img class="img-fluid" src="assets/img/player_img.png" alt="">
                 </a>
                 <div class="embed-responsive embed-responsive-16by9 hidden" id="playerContainer">
