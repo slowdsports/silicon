19fa1e91445829e30841a5458a94b9b76cc8eb6d
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-16 00:48:10 -0600

    Create banner.php

---
+++ inc/ads/banner.php
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    atOptions = {
+        'key': 'dc80d23b92278479e0b004998256c629',
+        'format': 'iframe',
+        'height': 250,
+        'width': 300,
+        'params': {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/dc80d23b92278479e0b004998256c629/invoke.js"></scr' + 'ipt>');
+</script>
