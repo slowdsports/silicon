edff26155002aa5d64d02c1dadea70cb3ee48990
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-22 22:04:51 -0600

    Update play.php

--- play.php
+++ play.php
@@ -106,7 +106,7 @@ if (isset($_GET['title'])) {
                         $src = "id='embed-player' class='embed-responsive-item' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen allow-encrypted-media src='inc/reproductor/{$config[0]}?{$params}'";
                     }
                     // Configurar los claro
-                    if (strpos($canalUrl, "clarovideo")) {
+                    if (strpos($canalUrl, "claro" )) {
                         $canalUrl = base64_encode($result['canalUrl']);
                         $key = $result['key'];
                         $key2 = $result['key2'];
