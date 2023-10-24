6ee3f8b73da5e558a64872629049b7f00631e44a
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-24 00:05:17 -0600

    Update play.php

--- play.php
+++ play.php
@@ -107,10 +107,7 @@ if (isset($_GET['title'])) {
                     }
                     // Configurar los claro
                     if (strpos($canalUrl, "claro")) {
-                        $canalUrl = base64_encode($result['canalUrl']);
-                        $key = $result['key'];
-                        $key2 = $result['key2'];
-                        $src = "id='embed-player' class='embed-responsive-item' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen allow='encrypted-media' src='//clarovideo.irtvhn.info?get=$canalUrl&key=$key&key2=$key2'";
+                        $src = "id='embed-player' class='embed-responsive-item' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen allow='encrypted-media' src='//clarovideo.irtvhn.info?c=$canalId'";
                     } elseif (isset($canalTipo) && isset($configurations[$canalTipo])) {
                         // Obtener el tipo de canal de la base de datos y verificar si existe en las configuraciones
                         $config = $configurations[$canalTipo];
