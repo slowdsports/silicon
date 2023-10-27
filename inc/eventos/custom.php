9702ff1e9d67c0481c2221fd62607bd942dfffb9
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-10-27 00:31:30 -0600

    Update custom.php

--- inc/eventos/custom.php
+++ inc/eventos/custom.php
@@ -1,12 +1,31 @@
 <?php
 switch ($index) {
-    case 11548617:
-        $custId = 11; break;
-    case 11548606:
-        $custId = 12; break;
+    case 11548601:
+        $custId = 19; break;
+    case 11548605:
+        $custId = 18; break;
+    case 11548613:
+        $custId = 22; break;
+    case 11548619:
+        $custId = 21; break;
+    case 11548621:
+        $custId = 20; break;
+    case 11548620:
+        $custId = 23; break;
+    case 11548623:
+        $custId = 24; break;
+    case 11548622:
+        $custId = 25; break;
+    case 11548624:
+        $custId = 26; break;
+    case 11548626:
+        $custId = 27; break;
+    case 11548627:
+        $custId = 28; break;
+    
 
         default:
-        $custId;; break;
+        $custId; break;
 }
 $custom = '<a class="justify-content-center list-group-item list-group-item-action"
         href="?p=tv&evento&nbalp='.$custId.'">
