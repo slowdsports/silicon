8ac3d4e197f4a157eb88c2d22174ed08e1e565b2
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-11-01 19:48:44 -0600

    Update sofa.php

--- admin/inc/componentes/back/sofa.php
+++ admin/inc/componentes/back/sofa.php
@@ -15,12 +15,11 @@ if ($apiLeague):
     $jsonData = json_decode($data, true);
     $seasonId = $jsonData['seasons'][0]['id'];
     // Games Info
-    if ($apiLeague == 11205) {
-        $apiUrl = "https://api.sofascore.com/api/v1/unique-tournament/" . $apiLeague . "/season/" . $seasonId . "/events/last/0";
-    } else {
-        $apiUrl = "https://api.sofascore.com/api/v1/unique-tournament/" . $apiLeague . "/season/" . $seasonId . "/events/next/0";
-    }
+    $apiUrl = "https://api.sofascore.com/api/v1/unique-tournament/" . $apiLeague . "/season/" . $seasonId . "/events/next/0";
     $data = file_get_contents($apiUrl);
+    if (!$data) {
+        $apiUrl = "https://api.sofascore.com/api/v1/unique-tournament/" . $apiLeague . "/season/" . $seasonId . "/events/last/0";
+    }
     $jsonData = json_decode($data, true);
     // Recorrer Data y guardar en DB
     foreach ($jsonData['events'] as $event) {
