c8eb641c31fc86d67bca90044055333ca24fe08b
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-11-19 04:23:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -24,10 +24,11 @@ if(isset($_GET['p'])) {
         include("404.php");
     }
 } elseif (isset($_GET['updateChannels'])) {
-    $sql = "SELECT f.`fuenteId`, f.`fuenteNombre`, f.`canal`, c.`canalImg`, c.`canalCategoria`, c2.`categoriaNombre`
+    $sql = "SELECT f.`fuenteId`, f.`fuenteNombre`, f.`canal`, f.`pais`,  c.`canalImg`, c.`canalCategoria`, c2.`categoriaNombre`, p.`paisNombre`
     FROM `fuentes` f
     JOIN `canales` c ON f.`canal` = c.`canalId`
-    JOIN `categorias` c2 ON c.`canalCategoria` = c2.`categoriaId`";
+    JOIN `categorias` c2 ON c.`canalCategoria` = c2.`categoriaId`
+    JOIN `paises` p ON f.`pais` = p.`paisId`";
     $result = $conn->query($sql);
     $canales = array();
     while ($row = $result->fetch_assoc()) {$canales[] = $row;}
