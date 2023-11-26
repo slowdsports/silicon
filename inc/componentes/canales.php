177a135ac3337ed698c34b23bce409a10f01a756
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-11-26 04:15:29 -0600

    Update canales.php

--- inc/componentes/canales.php
+++ inc/componentes/canales.php
@@ -2,6 +2,45 @@
 function mostrarCanalesFromJson($jsonData, $categoriaFiltro = null, $limit = null)
 {
     $canales = json_decode($jsonData, true);
+    $canalesCategoria22 = array_filter($canales, function ($canal) {
+        return $canal['categoriaNombre'] === 'Adultos';
+    });
+    if (isset($_GET['p']) && $_GET['p'] == "tv") { ?>
+        <div class="canal mycard col-6 col-md-4 col-lg-3 col-xl-2" data-category="22">
+            <a href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="card border-primary shadow-sm card-hover card-hover-gradient" aria-hidden="true">
+                    <div class="position-relative placeholder-wave">
+                        <div class="card-header">
+                            <img class="card-img-canal placeholder-wave" src="https://i.ibb.co/w0qg9JF/trans.png"
+                                alt="Card image"
+                                style="background-image: url('assets/img/canales/18.png'); background-size: contain; background-repeat: no-repeat;">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title placeholder-glow">
+                                Adultos
+                            </h5>
+                            <div class="card-footer fs-sm text-muted placeholder-glow">
+                                <i class="flag spain"></i>
+                                +18
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <div class="dropdown-menu my-1">
+                <a href="?p=tv&adultos" class="dropdown-item">
+                    VOD
+                </a>
+                <div class="dropdown-divider"></div>
+                <?php foreach ($canalesCategoria22 as $canalCategoria22) { ?>
+                <a href="?p=tv&c=<?= $canalCategoria22['canal'] ?>&f=<?= $canalCategoria22['fuenteId'] ?>" class="dropdown-item">
+                    <?= $canalCategoria22['fuenteNombre'] ?>
+                </a>
+                <?php } ?>
+            </div>
+        </div>
+        <?php
+    }
     $count = 0;
     foreach ($canales as $canal) {
         if ($categoriaFiltro && $canal['categoriaNombre'] !== $categoriaFiltro) {
@@ -18,16 +57,17 @@ function mostrarCanalesFromJson($jsonData, $categoriaFiltro = null, $limit = nul
         $canalImg = $canal['canalImg'];
         $canalNombre = $canal['fuenteNombre'];
         $canalCategoria = $canal['categoriaNombre'];
+        $categoria = $canal['canalCategoria'];
         $pais = $canal['paisNombre'];
         ?>
-        <div class="canal mycard col-6 col-md-4 col-lg-3 col-xl-2">
+        <div class="canal mycard col-6 col-md-4 col-lg-3 col-xl-2" data-category="<?= $categoria ?>">
             <a href="?p=tv&c=<?= $canalId ?>&f=<?= $fuenteId ?>">
                 <div class="card border-primary shadow-sm card-hover card-hover-gradient" aria-hidden="true">
                     <div class="position-relative placeholder-wave">
                         <div class="card-header">
                             <img class="card-img-canal placeholder-wave" src="https://i.ibb.co/w0qg9JF/trans.png"
-                            alt="Card image"
-                            style="background-image: url('assets/img/canales/<?= $canalImg ?>.png'); background-size: contain; background-repeat: no-repeat;">
+                                alt="Card image"
+                                style="background-image: url('assets/img/canales/<?= $canalImg ?>.png'); background-size: contain; background-repeat: no-repeat;">
                         </div>
                         <div class="card-body">
                             <h5 class="card-title placeholder-glow">
