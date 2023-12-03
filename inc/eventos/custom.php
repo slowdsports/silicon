e888280468107296bd9c8d56736ed1ef42d89778
slowdsports <72911530+slowdsports@users.noreply.github.com>
2023-12-03 10:11:41 -0600

    Update custom.php

--- inc/eventos/custom.php
+++ inc/eventos/custom.php
@@ -6,28 +6,28 @@ function getCustomLink($index) {
             return 5;
 
         // NFL
-        case 11275339:
-            return 16;
-        case 11275337:
-            return 15;
-        case 11275336:
-            return 12;
-        case 11275335:
+        case 11275304:
             return 17;
-        case 11275334:
-            return 14;
-        case 11275332:
-            return 13;
-        case 11275341:
-            return 19;
-        case 11275340:
-            return 18;
-        case 11275343:
-            return 20;
-        case 11275342:
-            return 21;
-        case 11275344:
+        case 11275346:
+            return 16;
+        case 11275325:
             return 22;
+        case 11275323:
+            return 18;
+        case 11275322:
+            return 20;
+        case 11275321:
+            return 19;
+        case 11275318:
+            return 21;
+        case 11275306:
+            return 23;
+        case 11275324:
+            return 25;
+        case 11274062:
+            return 24;
+        case 11275330:
+            return 26;
 
         // NBA
         case 11550576:
