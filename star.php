0d0247447337851addb7f0132c7b79aa62ed8fac
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-05-08 00:17:18 -0600

    svitar

--- star.php
+++ star.php
@@ -1,3 +1,16 @@
+<?php
+// Generar
+$json_url = 'https://deportestvhd2.com/star.json';
+// Obtener el contenido del JSON desde la URL
+$json_content = file_get_contents($json_url);
+// Verificar si la solicitud fue exitosa
+if ($json_content !== false) {
+    // Guardar el contenido en un archivo llamado "starbr.json"
+    file_put_contents('datos.json', $json_content);
+} else {
+    echo 'Error al obtener el contenido del JSON, por favor comuníquelo al admin';
+}
+?>
 <section class="container">
     <!-- Page title + Filters -->
     <div class="d-lg-flex align-items-center justify-content-between py-4 mt-lg-2">
