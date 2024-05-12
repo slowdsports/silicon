33963f74650cafc7ccda72b14e430b5e7bc8b2e0
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-05-12 01:18:52 -0600

    Create promo.php

---
+++ inc/componentes/promo.php
@@ -0,0 +1,80 @@
+<section class="container pb-sm-1 pb-md-3">
+    <!-- Row 1 -->
+    <div class="row align-items-lg-center pb-5 mb-2 mb-lg-4 mb-xl-5">
+        <div class="col-md-6 mb-4 mb-md-0">
+            <blockquote class="twitter-tweet rounded-3" data-media-max-width="560">
+                <p lang="fr" dir="ltr">MERCI. 🔴🔵 <a
+                        href="https://twitter.com/PSG_inside?ref_src=twsrc%5Etfw">@PSG_inside</a> <a
+                        href="https://t.co/t0cL2wPpjX">pic.twitter.com/t0cL2wPpjX</a></p>&mdash; Kylian Mbappé
+                (@KMbappe) <a href="https://twitter.com/KMbappe/status/1788991573029712287?ref_src=twsrc%5Etfw">May 10,
+                    2024</a>
+            </blockquote>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+        <div class="col-md-6">
+            <div class="ps-xl-5 ms-md-2 ms-lg-4">
+                <h2 class="h1 mb-3 mb-sm-4">Mbappé jugará su último partido con el PSG</h2>
+                <p class="mb-4 mb-lg-5">El jugador anunció que el juego contra el Toulouse de la Ligue 1 será el último partido en el Parque de los Príncipes.
+                Mirá el partido en directo</p>
+                <a style="text-decoration: none" href="?p=eventos&tipo=football&liga=34&juego=11391267">
+                    <div class="card product-card">
+                        <div class="main-event">
+                            <div class="league">
+                                <img src="assets/img/ligas/sf/34.png" alt="League">
+                                <!-- <p class="11391267"></p> -->
+                                <p class="fs-sm text-body mb-0 cntdwn-11391267">11h 47m 53s</p>
+                            </div>
+                            <div class="match">
+                                <div class="team">
+                                    <img width="60px" src="assets/img/equipos/sf/1644.png" alt="">
+                                    <h4>
+                                        Paris Saint-Germain </h4>
+                                </div>
+                                <div class="vs">
+                                    <h6>vs</h6>
+                                </div>
+                                <div class="team">
+                                    <img width="60px" src="assets/img/equipos/sf/1681.png" alt="">
+                                    <h4>
+                                        Toulouse </h4>
+                                </div>
+                            </div>
+                            <div class="channel">
+                                <img src="assets/img/canales/eurosport.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Row 2 -->
+    <div style="display: none;" class="row align-items-lg-center pt-md-3 pb-5 mb-2 mb-lg-4 mb-xl-5">
+        <div class="col-md-6 order-md-2 mb-4 mb-md-0">
+            <img src="assets/img/services/single/image02.jpg" class="rounded-3" alt="Image">
+        </div>
+        <div class="col-md-6 order-md-1">
+            <div class="pe-xl-5 me-md-2 me-lg-4">
+                <h2 class="h1 mb-3 mb-sm-4">Product development with the best experience</h2>
+                <p class="mb-3 mb-sm-4">We create diverse, complex, web and mobile solutions for any business need. With
+                    us you get quality software and perfect service every time. Quisque integer eget velit facilisis
+                    enim malesuada metus, risus amet ultricies.</p>
+                <ul class="list-unstyled mb-0">
+                    <li class="d-flex mb-2">
+                        <i class="bx bx-check text-primary lead me-2"></i>
+                        Quis euismod lacus, at consectetur porta
+                    </li>
+                    <li class="d-flex mb-2">
+                        <i class="bx bx-check text-primary lead me-2"></i>
+                        Dictumst enim lectus dis eget non metus cras
+                    </li>
+                    <li class="d-flex">
+                        <i class="bx bx-check text-primary lead me-2"></i>
+                        Risus volutpat tellus hendrerit nibh
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
