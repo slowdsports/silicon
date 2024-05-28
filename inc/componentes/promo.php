0aff3cb2dec9a3f630d0b9b7d87acea743b11fdb
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-05-28 16:00:45 -0600

    Update promo.php

--- inc/componentes/promo.php
+++ inc/componentes/promo.php
@@ -6,7 +6,7 @@
                 style="border-radius: 15px;"
                 width="100%"
                 height="315"
-                src="https://www.youtube.com/embed/0fVpYLQi8GU?si=bR8IXIbiG5BI_2aC&autoplay=1&mute=1&loop=1"
+                src="https://www.youtube.com/embed/wljsbPQR-gQ?si=bR8IXIbiG5BI_2aC&autoplay=1&mute=1&loop=1"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
