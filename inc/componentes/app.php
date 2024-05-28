9237a5ca3e6263ef74f8683f03422c8ee03403b4
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-05-28 14:33:42 -0600

    Create app.php

---
+++ inc/componentes/app.php
@@ -0,0 +1,11 @@
+<!-- App Store button -->
+<a href="https://www.highcpmgate.com/uenxw914?key=538cc18fac4f254bb01f2e1e0e0ccc3a" class="btn btn-dark btn-lg px-3 py-2">
+  <img src="assets/img/market/appstore-light.svg" class="light-mode-img" width="124" alt="App Store">
+  <img src="assets/img/market/appstore-dark.svg" class="dark-mode-img" width="124" alt="App Store">
+</a>
+
+<!-- Google Play button -->
+<a href="https://www.highcpmgate.com/uenxw914?key=538cc18fac4f254bb01f2e1e0e0ccc3a" class="btn btn-dark btn-lg px-3 py-2">
+  <img src="assets/img/market/googleplay-light.svg" class="light-mode-img" width="139" alt="Google Play">
+  <img src="assets/img/market/googleplay-dark.svg" class="dark-mode-img" width="139" alt="Google Play">
+</a>
