c1d1960a797b6479e9155e3c1a5987a5b049000f
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-05-30 16:39:44 -0600

    Update tv.php

--- tv.php
+++ tv.php
@@ -1,4 +1,6 @@
 <?php
+$titulo = "Ver canales de televisión en vivo y en directo";
+$descripcion = "Ver televisión por internet completamente gratis y en alta definición por iRaffle TV.";
 // Reproductor
 if (isset($_GET['c']) || isset($_GET['r'] )|| isset($_GET['s'] )|| isset($_GET['v'] )|| isset($_GET['key'] )|| isset($_GET['evento'] )|| isset($_GET['ifr'] ) || isset($_GET['m3u'] )) {
     include('play.php');
