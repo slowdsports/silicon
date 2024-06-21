9edd73cd480fbc5a5d47d6aaa48b1d5be6bf3bdd
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-06-21 11:05:29 -0600

    Update star.php

--- star.php
+++ star.php
@@ -39,9 +39,8 @@ if ($json_content !== false) {
                         <img class="img-fluid" src="assets/img/player_img.png" alt="">
                     </a>
                     <div class="embed-responsive embed-responsive-16by9" id="playerContainer">
-                        <iframe id='embed-player' class='embed-responsive-item' width='100%' height='100%'
-                            frameborder='0' scrolling='no' allowfullscreen allow-encrypted-media></iframe>
-                    </div>
+                        <iframe allow="encrypted-media" allowfullscreen id='embed-player' class='embed-responsive-item' width='100%' height='100%'
+                            frameborder='0' scrolling='no'></iframe>
                 </div>
             </div>
         </div>
