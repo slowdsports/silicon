6c8af5e538e530acad8eab972e2d8d9bc64ffa6e
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-07-08 07:57:50 -0600

    Update popunder.php

--- inc/ads/popunder.php
+++ inc/ads/popunder.php
@@ -1,1 +1,1 @@
-<script type='text/javascript' src='//pl23429035.highcpmgate.com/a5/97/86/a597865b8f7dea74a0e3d18eb7f0daec.js'></script>
+<!-- <script type='text/javascript' src='//pl23429035.highcpmgate.com/a5/97/86/a597865b8f7dea74a0e3d18eb7f0daec.js'></script> -->
