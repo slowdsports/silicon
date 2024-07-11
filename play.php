47a5fb82f7da0797215175a8cf61beef999fcc1d
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-07-11 16:11:12 -0600

    Update play.php

--- play.php
+++ play.php
@@ -192,7 +192,7 @@ elseif (isset($_GET['id'])) {
                     } else {
                         // Configurar los claro && Flow
                         if (strpos($canalUrl, "claro") || strpos($canalUrl, "cvatt")) {
-                            $src = "id='embed-player' class='embed-responsive-item' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen allow='encrypted-media' src='//clarovideo.irtvhn.info?c=$canalAlt'";
+                            $src = "id='embed-player' class='embed-responsive-item' width='100%' height='100%' frameborder='0' scrolling='no' allowfullscreen allow='encrypted-media' src='//clarovideo.futbolhonduras24.com?c=$canalAlt'";
                         }
                         // Configurar los IZZI
                         elseif (strpos($canalUrl, "izzigo")) {
