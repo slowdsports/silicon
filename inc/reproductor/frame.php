c81a458b8a39629e5df4c7bd0a4079932593a923
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-07-14 12:09:21 -0600

    Update frame.php

--- inc/reproductor/frame.php
+++ inc/reproductor/frame.php
@@ -41,7 +41,7 @@ if (isset($_GET['f']) && $_GET['f'] !== null) {
 
 // Especiales
 $sandbox;
-if (strpos($canalUrl, "bletcheanta") || strpos($canalUrl, "now.net") || strpos($canalUrl, "dlhd")) {
+if (strpos($canalUrl, "bletcheanta") || strpos($canalUrl, "now.net") || strpos($canalUrl, "dlhd") || strpos($canalUrl, "betzta")) {
     $sandbox = "sandbox='allow-same-origin allow-scripts'";
 }
 ?>
