24d9f31137655f1ed99b16dc9d624e5c76a057db
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-07-15 09:42:48 -0600

    Update intersticial.php

--- inc/ads/intersticial.php
+++ inc/ads/intersticial.php
@@ -1,14 +1,14 @@
 <?php
 // Script A
-//$ad_script_a = "<script type='text/javascript' src='//pl23429035.highcpmgate.com/a5/97/86/a597865b8f7dea74a0e3d18eb7f0daec.js'></script>";
+$ad_script_a = "<script type='text/javascript' src='//pl23429035.highcpmgate.com/a5/97/86/a597865b8f7dea74a0e3d18eb7f0daec.js'></script>";
 
 // Script B
-//$ad_script_b = "<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://arvigorothan.com/tag.min.js',7512906,document.body||document.documentElement)</script>";
+$ad_script_b = "<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://arvigorothan.com/tag.min.js',7512906,document.body||document.documentElement)</script>";
 
-//$random_number = mt_rand(0, 99);
-//if ($random_number < 70) {
-//    echo $ad_script_b;
-//} else {
-//    echo $ad_script_a;
-//}
+$random_number = mt_rand(0, 99);
+if ($random_number < 70) {
+    echo $ad_script_b;
+} else {
+    echo $ad_script_a;
+}
 ?>
