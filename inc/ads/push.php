d38ffaaa9e941e9651f362f76741fffb7b9b2376
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-07-15 09:43:39 -0600

    Update push.php

--- inc/ads/push.php
+++ inc/ads/push.php
@@ -1,1 +1,1 @@
-<!-- <script data-cfasync="false" src="//asccdn.com/script/ippg.js" data-adel="inpage" zid="7721606" rr="30" mads="2"></script> -->
+<script data-cfasync="false" src="//asccdn.com/script/ippg.js" data-adel="inpage" zid="7721606" rr="30" mads="2"></script>
