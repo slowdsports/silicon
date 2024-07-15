d9e4502d899da6107203c069bdda128472cc166b
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-07-15 15:43:56 -0600

    Update promo.php

--- inc/componentes/promo.php
+++ inc/componentes/promo.php
@@ -1,9 +1,10 @@
-<section class="container pb-sm-1 pb-md-3">
-    <div class"row">
-        <div class"col-12 text-center">
-            <blockquote class="twitter-tweet"><p lang="es" dir="ltr">[AHORA] Liberaron al creador de Fútbol Libre: la Justicia lo excarceló por &quot;colaborar con la investigación contra los sitios de piratería&quot;. <a href="https://t.co/W4ekowk5y9">https://t.co/W4ekowk5y9</a> <a href="https://t.co/f8GOGujT6v">pic.twitter.com/f8GOGujT6v</a></p>&mdash; ElCanciller.com (@elcancillercom) <a href="https://twitter.com/elcancillercom/status/1812944836556910754?ref_src=twsrc%5Etfw">July 15, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
+<div class"container text-center pt-5 mt-2 mt-md-4 mt-lg-5">
+    <div class"h1 pt-2 pt-lg-0">
+        <blockquote class="twitter-tweet"><p lang="es" dir="ltr">[AHORA] Liberaron al creador de Fútbol Libre: la Justicia lo excarceló por &quot;colaborar con la investigación contra los sitios de piratería&quot;. <a href="https://t.co/W4ekowk5y9">https://t.co/W4ekowk5y9</a> <a href="https://t.co/f8GOGujT6v">pic.twitter.com/f8GOGujT6v</a></p>&mdash; ElCanciller.com (@elcancillercom) <a href="https://twitter.com/elcancillercom/status/1812944836556910754?ref_src=twsrc%5Etfw">July 15, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
+</div>
+
+<section class="container pb-sm-1 pb-md-3">
     <!-- Row 1 -->
     <div style="display: none" class="row align-items-lg-center pb-5 mb-2 mb-lg-4 mb-xl-5">
         <div class="col-md-6 mb-4 mb-md-0">
