fea35a534355fb0d5a33a09eb134ddd7ee573822
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-08-05 16:01:26 -0600

    Update promo.php

--- inc/componentes/promo.php
+++ inc/componentes/promo.php
@@ -1,19 +1,3 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-            <blockquote class="twitter-tweet">
-                <p lang="es" dir="ltr">
-                    [AHORA] Liberaron al creador de Fútbol Libre: la Justicia lo excarceló por "colaborar con la investigación contra los sitios de piratería".
-                    <a href="https://t.co/W4ekowk5y9">https://t.co/W4ekowk5y9</a> 
-                    <a href="https://t.co/f8GOGujT6v">pic.twitter.com/f8GOGujT6v</a>
-                </p>&mdash; ElCanciller.com (@elcancillercom) 
-                <a href="https://twitter.com/elcancillercom/status/1812944836556910754?ref_src=twsrc%5Etfw">July 15, 2024</a>
-            </blockquote>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-    </div>
-</div>
-
 <section class="container pb-sm-1 pb-md-3">
     <!-- Row 1 -->
     <div style="display: none" class="row align-items-lg-center pb-5 mb-2 mb-lg-4 mb-xl-5">
