e994382c9c05d3c053967f97e6f113b606979b20
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-10-08 10:08:01 -0600

    Create validar.php

---
+++ inc/usuario/validar.php
@@ -0,0 +1,20 @@
+<?php
+function validarSuscripcion() {
+    if (!isset($_COOKIE['usuario_id']) || !isset($_COOKIE['usuario_rol'])) {
+        return false;  // No está logueado o no es VIP
+    } else {
+        include('../conn.php');
+        $usId = $_COOKIE['usuario_id'];
+        $usQuery = mysqli_query($conn, "SELECT id, rol_id, suscripcion FROM usuarios WHERE id = '$usId'");
+        $result = mysqli_fetch_array($usQuery);
+        $suscripcion = $result['suscripcion'];
+        $fecha_actual = new DateTime();
+        $fecha_suscripcion = new DateTime($suscripcion);
+        if ($fecha_suscripcion > $fecha_actual) {
+            return true;  // Suscripción activa
+        } else {
+            return false;  // Suscripción ha expirado
+        }
+    }
+}
+?>
