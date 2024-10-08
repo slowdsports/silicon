0f42bb8733a4860dae3e02a27bb224fcbbc91be4
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-10-08 10:20:03 -0600

    Update validar.php

--- inc/usuario/validar.php
+++ inc/usuario/validar.php
@@ -3,7 +3,7 @@ function validarSuscripcion() {
     if (!isset($_COOKIE['usuario_id']) || !isset($_COOKIE['usuario_rol'])) {
         return false;  // No está logueado o no es VIP
     } else {
-        include('../conn.php');
+        include('/home/u5233116/web/inc/conn.php');
         $usId = $_COOKIE['usuario_id'];
         $usQuery = mysqli_query($conn, "SELECT id, rol_id, suscripcion FROM usuarios WHERE id = '$usId'");
         $result = mysqli_fetch_array($usQuery);
