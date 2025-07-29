1424ab94e5d6fa40df4573e4d0234d4e447cc333
slowdsports <72911530+slowdsports@users.noreply.github.com>
2025-07-29 15:51:09 -0600

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -1,7 +1,7 @@
 <?php
 //$conn = mysqli_connect("localhost","megaaxmg","UEkpjZOIzDnJk","megaaxmg_matte");
 //$conn = mysqli_connect("localhost","root","root","besoccer"); //local
-$conn = mysqli_connect("localhost","u5233116_n57whzfm2yvo","SlowD2019","u5233116_besoccer"); //localHost
+$conn = mysqli_connect("localhost","u5855787_root","SlowD2019","u5855787_besoccer"); //localHost
 //$conn = mysqli_connect("localhost","iraffle1_root","z1CnB-*XSJ-W","iraffle1_besoccer"); //prod
 
 // Check connection
